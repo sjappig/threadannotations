a94989fc1dc680fa669bfe0072f68a022f51ef4a
Jari-Pekka Ryynänen <ryynanej@kapsi.fi>
2015-06-28 20:42:35 +0300

    Use SLF4J as logger
    This allows library user to configure logging.

--- taagent/src/main/java/com/github/sjappig/ta/TAAgent.java
+++ taagent/src/main/java/com/github/sjappig/ta/TAAgent.java
@@ -2,10 +2,15 @@ package com.github.sjappig.ta;
 
 import java.lang.instrument.Instrumentation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TAAgent {
 
-	public static void premain(String args, Instrumentation inst) {
+	private static Logger log = LoggerFactory.getLogger(TAAgent.class);
 
+	public static void premain(String args, Instrumentation inst) {
+		log.debug("Entered java-agent premain");
 	}
 
 }
